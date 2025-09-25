86792de5c4a9bf013404ab723541d1b8ed9db384
Josh Koter <josh.koter@gmail.com>
2025-09-25 18:08:29 +0200

    ...

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,9 +3,7 @@ namespace BlazingCrud.Models;
 public class Student
 {
     public int Id { get; set; }
-
     public string? Name { get; set; }
-
     public int Age { get; set; }
     public DateTime? Birthday { get; set; }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+here: https://youtu.be/CpbRAWgFBRQ?si=bW-GTXzbqxp-7HZE&t=3583
